c33bd2c54565c822bd7f3b0bdfcc0768366e54b9
Jay Kania <jaykania123@gmail.com>
2022-09-11 19:42:53 -0300

    added header

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,10 +1,37 @@
 import { useRouter } from "next/router";
 import React from "react";
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
+import { getEventById } from "../../dummy-data";
 
 const SingleEvent = () => {
   const router = useRouter();
-  console.log(router.query);
-  return <h2>Single Event</h2>;
+  console.log(router.query.eventId);
+  const eventId = router.query.eventId;
+
+  const event = getEventById(eventId);
+
+  console.log(event);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 };
 
 export default SingleEvent;
